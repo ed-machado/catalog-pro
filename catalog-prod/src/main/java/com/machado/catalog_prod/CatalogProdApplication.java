90ae86ea3fd1a7f7b06faa08f196db70578ebb5a
ed-machado <louchi.machado@gmail.com>
2024-11-14 19:35:18 -0300

    chore: initialize project structure with backend and frontend directories

---
+++ catalog-prod/src/main/java/com/machado/catalog_prod/CatalogProdApplication.java
@@ -0,0 +1,13 @@
+package com.machado.catalog_prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogProdApplication.class, args);
+	}
+
+}
